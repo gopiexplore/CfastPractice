79a3c949ea7da52bae840d25be34de287c5e4043
gopii8 <gopithammisetti6@gmail.com>
2023-02-26 07:27:51 +0530

    Greatest Number Among Two

---
+++ greatestamongtwo.c
@@ -0,0 +1,11 @@
+//Greatest of two numbers: 
+#include <stdio.h>
+void main(){
+    int a,b;
+    printf("Enter Two Numbers to find Greatest of Two");
+    scanf("%d %d",&a,&b);
+    if( a>b){
+        printf("%d is Greatest",a);
+        
+    }else printf("%d is Greatest ",b);
+}
